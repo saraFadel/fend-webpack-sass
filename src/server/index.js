64c3b6aaae299c20e8ac3f1cee7f13b979fcb4a2
SaraFadel <sara.elsalhany@gmail.com>
2021-11-04 07:37:27 +0200

    u

--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,23 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var bodyParser = require('body-parser')
+//var bodyParser = require('body-parser')
 var cors = require('cors')
 
-var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
-
 const app = express()
+
 app.use(cors())
-// to use json
-app.use(bodyParser.json())
-// to use url encoded values
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: false
+ }))
 
-app.use(express.static('dist'))
 
-console.log(JSON.stringify(mockAPIResponse))
+ app.get('/',((req, res) => {
+    res.json({
+        message: 'Catch You!',
+    })
+ }))
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
-
-app.get('/test', function (req, res) {
-    res.json(mockAPIResponse);
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
+ app.listen(8081, () => {
     console.log('Example app listening on port 8081!')
 })
